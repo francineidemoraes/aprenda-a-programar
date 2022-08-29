f59a06b0ffed5a36ea1b516cd8bc7e93cacdbdaa
Francineide Moraes <francineidemoraes@gmail.com>
2022-08-29 18:57:35 -0300

    primeiro programa

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,13 @@
-let primeiro nome = "Francineide"
+let nome = "Francineide"
 let email = "francineidemoraes@gmail.com"
 let = profissao = "Servidora publica"
 let idade =  46 
-let lugar que sonho conher = "Paris"
+let lugar = "Paris"
+let estado = "Rio de Janeiro"
+let frase = "Ola!"
+console.log (nome)
+console.log (email)
+console.log (profissao)
+console.log (idade)
+console.log (lugar)
+console.log (estado)
