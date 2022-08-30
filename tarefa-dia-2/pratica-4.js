2c7400ea975eade3b380bd4bc0c7856258a1367e
Francineide Moraes <francineidemoraes@gmail.com>
2022-08-30 19:36:59 -0300

    primeiro programa

---
+++ tarefa-dia-2/pratica-4.js
@@ -0,0 +1,30 @@
+
+//objetos
+//let produtoA = {"nome": "camiseta", "valor": 100.00, "internacional": true};
+//let produtoB = {"nome": "perfume", "valor": 200.00, "internacional": true};
+//let produtoC = {"nome": "sandália", "valor": 120.00, "internacional": false};
+
+var produtoA = {
+nome : "Camiseta", 
+valor : 100.00,
+internacional : true
+
+
+};
+
+var produtoB = { 
+nome : Perfume,
+valor : 200.00,
+internacional : true
+
+};
+
+var produtoC = {
+nome : perfume,
+valor : 120.00,
+internacional : false
+
+let = (produtoA, produtoB, produtoC);
+
+for (produtos of produtos)
+}
